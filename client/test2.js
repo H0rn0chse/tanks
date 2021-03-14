2e53f4a4f8615ef28fbe481ec68011de6efa6589
H0rn0chse <h0rn0chse@gmx.de>
2021-03-14 20:45:17 +0100

    Added tank sprite and gravity

--- client/test2.js
+++ client/test2.js
@@ -7,27 +7,42 @@ const sceneHeight = 500;
 class MainScene extends Scene {
     preload () {
         this.load.image("background", "assets/background.png");
+        this.load.image("tank", "assets/tank.png");
     }
 
     create () {
         this.addBackground();
         this.addGround();
-        this.addPlayerSprite();
+        this.addPlayer();
     }
 
-    addPlayerSprite () {
+    addPlayer () {
+        const player = new GameObjects.Sprite(this, 200, 0, "tank");
+        this.add.existing(player);
 
+        this.physics.add.existing(player);
+        this.physics.add.collider(player, this.ground);
+
+        player.body.collideWorldBounds = true;
     }
 
     addGround () {
         const simplex = new SimplexNoise();
+        this.ground = new GameObjects.Group(this, [], {});
         for (let x = 0; x < sceneWidth; x++) {
             const noiseWidth = 150;
             const maxHeight = 400;
             const minHeight = 100;
-            const height = (simplex.noise2D(0, x / noiseWidth) + 1) * (maxHeight - minHeight) + minHeight;
+            const height = (simplex.noise2D(0, x / noiseWidth) + 1) * ((maxHeight - minHeight) / 2) + minHeight;
+
             const rect = new GameObjects.Rectangle(this, x, sceneHeight, 1, height, 0xff0000);
+            this.ground.add(rect);
             this.add.existing(rect);
+            this.physics.add.existing(rect);
+
+            rect.body.collideWorldBounds = true;
+            rect.body.immovable = true;
+            rect.body.allowGravity = false;
         }
     }
 
